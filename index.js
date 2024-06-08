3d013679ee4adaf88f8d4755c7eec1417ad95c63
Alex Riviere <fimion@gmail.com>
2024-06-08 11:35:41 -0400

    better exports

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-throw new Error("Don't import this file!");
+export { default as PrettierConfig } from "./prettier.config.js";
+export { default as EsLintConfig } from "./eslint.config.js";
+export { default as EsLintTsConfig } from "./eslint-ts.config.js";
