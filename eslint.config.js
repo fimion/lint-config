ce4e5fe6dff22799b3dba17909a2e018cf25b2be
Alex Riviere <fimion@gmail.com>
2024-06-08 10:56:16 -0400

    better vue setup

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,23 @@ import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 /**
  * @type {import("eslint").ESLint.Options}
  */
-const rules = [...pluginVue.configs["flat/strongly-recommended"], eslintPluginPrettier];
+const rules = [
+	...pluginVue.configs["flat/strongly-recommended"],
+	{
+		/**
+		 * Better Vue Setup
+		 */
+		files: ["**/*.vue"],
+		rules: {
+			"vue/block-order": [
+				"error",
+				{
+					order: ["script:not([setup])", "script[setup]", "template", "style[scoped]", "style:not([scoped])"],
+				},
+			],
+		},
+	},
+	eslintPluginPrettier,
+];
 
 export default rules;
